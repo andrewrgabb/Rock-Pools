d9d5aae4235e9ce490548fe33d0ad9c47c5e0dac
Andrew Robert Gabb <andrew.gabb@192-168-1-9.tpgi.com.au>
2018-02-16 18:35:27 +1100

    Initial Commit

---
+++ Rock PoolsTests/Rock_PoolsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rock_PoolsTests.swift
+//  Rock PoolsTests
+//
+//  Created by Andrew Robert Gabb on 16/2/18.
+//  Copyright © 2018 Andrew Robert Gabb. All rights reserved.
+//
+
+import XCTest
+@testable import Rock_Pools
+
+class Rock_PoolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
